218bfec0fa42fefa20198b0f7ef8e072984bb97a
Andy Cao <caojundan@gmail.com>
2021-11-15 21:11:39 +1300

    feat: add youdao lib

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+import Translate from '@liqiqiang/youdao-translate';
 import alfy from 'alfy';
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+const translate = new Translate('525100d8f9304067', 'MsLWIHzModLvvgJyeXVFa5WoqgASoOR4');
 
-async function init() {
-  const data = await alfy.fetch(url);
+// auto en to zh
+let res = await translate.t(alfy.input);
+// console.log('res', res);
+// auto zh to en
+// res = await translate.t('你好，世界');
+// specified translate action
+// res = await translate.t('hello world', { from: 'EN', to: 'zh-CHS' })
 
-  const items = alfy.inputMatches(data, 'title')
-    .map(item => ({
-      title: item.title,
-      subtitle: item.body,
-      arg: item.arg,
-    }));
-  alfy.output(items);
-}
-
-//init()
-
-function demo() {
+if (res) {
+  const title = res.translation.join(', ')
+  let subtitle = ''
+  if (res.basic.explains){
+    subtitle = res.basic.explains.join(';\n ')
+  }
+  
   alfy.output([{
-    title: 'Unicorn',
+    title,
+    subtitle
+  }]);
+
+} else {
+  alfy.output([{
+    title: 'Cannot find any result',
     subtitle: alfy.input
   }]);
 }
 
-demo();
+
+
+
