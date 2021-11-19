c775469b65fee988e15358f66573f8ce2c8a70d2
Andy Cao <caojundan@gmail.com>
2021-11-20 06:16:07 +1300

    feat: add .env

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 // this means it running outside of the alfred
 function log(...rest) {
   if (!process.env.alfred_version) {
-    console.log('dev log: ', rest);
+    console.log('dev log: ', ...rest);
   }
 }
 
