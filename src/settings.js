542e8b94c351ccbd14980f597e711e8806858a02
Andy <andycao@yeah.net>
2021-11-22 22:52:19 +1300

    Fix/cannot export usable pack (#6)
    * 1.0.4
    
    * 1.0.5
    
    * 1.0.6
    
    * 1.0.7
    
    * 1.0.8
    
    * fix: move vars to settings
    
    Co-authored-by: Andy Cao <caojundan@gmail.com>

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,3 @@
-export const APP_KEY = '525100d8f9304067'
-export const APP_SECRET = 'MsLWIHzModLvvgJyeXVFa5WoqgASoOR4'
 // if set it as true, the workflow will show some extra information
 // when user using it
 export const DEBUG_MODE = false;
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
+import alfy from "alfy";
 
 // this means it running outside of the alfred
+// don't output any information inside the alfred env
 function log(...rest) {
   if (!process.env.alfred_version) {
     console.log('dev log: ', ...rest);
